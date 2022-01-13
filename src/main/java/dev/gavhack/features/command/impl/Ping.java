0d8d49311fdb06bb83aa287593234137594929e5
Sxmurai <gavrw0607@gmail.com>
2022-01-12 21:20:37 -0600

    cleanup Ping command

--- src/main/java/dev/gavhack/features/command/impl/Ping.java
+++ src/main/java/dev/gavhack/features/command/impl/Ping.java
@@ -2,11 +2,9 @@ package dev.gavhack.features.command.impl;
 
 import dev.gavhack.features.command.Command;
 import dev.gavhack.util.internal.ChatUtil;
-import net.minecraft.src.GuiPlayerInfo;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 public class Ping extends Command {
     public Ping() {
@@ -15,15 +13,6 @@ public class Ping extends Command {
 
     @Override
     public void run(List<String> args) {
-        int latency = 0;
-
-        Optional<GuiPlayerInfo> info = mc.getNetHandler().playerInfoList.stream()
-                .filter((inf) -> inf.name.equals(mc.thePlayer.getEntityName()))
-                .findFirst();
-        if (info.isPresent()) {
-            latency = info.get().responseTime;
-        }
-
-        ChatUtil.sendPrefixed(latency + "ms");
+        ChatUtil.sendPrefixed(getGavhack().getServerManager().getLatency(mc.thePlayer.getEntityName()) + "ms");
     }
 }
